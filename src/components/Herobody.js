a19bbc72feddc1d506096676feee67dc52a4e20c
ShaikMaimoon <shaikmaimoon09@gmail.com>
2024-06-24 16:46:29 +0530

    Changed

--- src/components/Herobody.js
+++ src/components/Herobody.js
@@ -1,23 +1,24 @@
-import React, { useEffect,useRef } from 'react'
-import Typed from 'typed.js';
-import pdf from '../pdf/Shaik Maimoon_resume2j.pdf'
+//import React, { useEffect,useRef } from 'react'
+import React from 'react'
+//import Typed from 'typed.js';
+import pdf from '../pdf/Shaik Maimoon_resume.pdf'
 export default function HeroBody() {
-    const typedRef=useRef(null)
-    useEffect(() =>{
-        const options={
-        strings:[
-            "Welcome to My Profile",
-            "My Name is Shaik Maimoon",
-        ],
-        typeSpeed: 50,
-        backSpeed:50,
-        loop:true,
-    };
-    const typed=new Typed(typedRef.current,options)
-    return ()=>{
-        typed.destroy()
-    }
-    },[])
+    // const typedRef=useRef(null)
+    // useEffect(() =>{
+    //     const options={
+    //     strings:[
+    //         "Welcome to My Profile",
+    //         "My Name is Shaik Maimoon",
+    //     ],
+    //     typeSpeed: 50,
+    //     backSpeed:50,
+    //     loop:true,
+    // };
+    // const typed=new Typed(typedRef.current,options)
+    // return ()=>{
+    //     typed.destroy()
+    // }
+    // },[])
   return (
     <>
     <section>
@@ -30,8 +31,8 @@ export default function HeroBody() {
                         <h4 data-aos="fade-right" data-aos-duration="1000">Graduate</h4>
                     </div>
                     <div id="intro">
-                        {/* <h1>Hi iam Shaik Maimoon</h1> */}
-                        <h1 ref={typedRef}></h1>
+                        <h1>Hi iam Shaik Maimoon</h1>
+                        {/* <h1 ref={typedRef}></h1> */}
                         
                         <h6>👋 Welcome to my digital space!  As a fresh graduate, I bring a blend of enthusiasm, creativity, and a
                             hunger to learn. Join me as I transform ideas into reality and grow as a professional. Let's
@@ -47,6 +48,28 @@ export default function HeroBody() {
                     </div>
                 </div>
                 
+            </div>
+        </div>
+    </section>
+    <section class="up-exp">
+        <div class="experience"
+         data-aos="fade-right">
+            <div class="section-title">
+                <h1>Experience</h1>
+            </div>
+            <div class="exp-detail">
+                <div class="company-name">
+                    <h1 class="reveal-right"  data-aos="fade-right">HR LABS</h1>
+                    <p class="reveal-right"  data-aos="fade-right">Frontend Developer Intern</p>
+                    <p class="reveal-right"  data-aos="fade-right">April, 2024 - June, 2024</p>
+                </div>
+                <div class="job-des reveal-left">
+                    <p class="reveal-left"  data-aos="fade-left">Where I have created a static website using HTML, CSS, React and hosted
+                        the website.</p>
+                    <a href="https://hrlabs.in/" target="_blank" class="reveal-left" data-aos="fade-left">Website Link <i
+                            class="fa-solid fa-arrow-up"></i></a>
+                             
+                </div>
             </div>
         </div>
     </section>
@@ -100,12 +123,12 @@ export default function HeroBody() {
                 <h1>Skills</h1>
                 <div>
                     <ul>
-                        <li><img src="Java_logo.png" alt="Java logo" className="logo"/>Java</li>
+                        <li><img src="Java.png" alt="Java logo" className="logo"/>Java</li>
                         <li><img src="logo192.png" alt="React Logo" className="logo"/>React</li>
                         <li><img src="html_logo.png" alt="HTML logo" className="logo"/>HTML</li>
                         <li><img src="css.png" alt="Css logo" className="logo"/>CSS</li>
                         <li><img src="js_logo.png" alt="JavaScript logo" className="logo"/>JavaScript</li>
-                        <li><img src="mysql_logo.png" alt="Mysql logo" className="logo"/>SQL</li>
+                        {/* <li><img src="mysql_logo.png" alt="Mysql logo" className="logo"/>SQL</li> */}
                     </ul>
                 </div>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,10 +18,16 @@ export default function Projects() {
                 data-aos="zoom-out"
                 data-aos-duration="1000">
                     <div>
-                        <FaPython id="react-icon"></FaPython>
+                        {/* <FaPython id="react-icon"></FaPython> */}
+                        <a href="https://drive.google.com/file/d/1Fj_cg2_IhBVj9Y-gL8eeP9luNJoDIQqL/view?usp=sharing" target="_blank" rel="noreferrer"><FaPython id="react-icon"/></a>
                         <h5>Prediction of Cardiovascular Disease by Using Hybrid model of Machine Learning Algorithms </h5>
-                        <p>This model enhances the prediction of cardiovascular illness prediction by 
-                            combining different algorithms such as decision tree, random forestand XGboost using ensemble laerning.
+                        <p>o Utilizing a hybrid Model of Machine Learning algorithms enhances cardiovascular illness prediction 
+                            by combining different algorithms, using its ensemble learning for improved accuracy.<br />
+                            o Compared different machine learning techniques like Decision Tree, Random Forest, XGboost
+                             to assess their effectiveness in heart disease prediction.<br />
+                            o This innovative approach combines models, resulting in a more accurate and efficient
+                             predictive system and also optimizes the errors.
+
                         </p>
                     </div>
                 </div>
@@ -29,11 +35,32 @@ export default function Projects() {
                 data-aos="zoom-out"
                 data-aos-duration="1000">
                     <div>
-                        <FaBookAtlas id="react-icon"/>
-                        <h5>A simple online book store(Minor-Project)</h5>
-                        <p>Created a simple online book store project 
-                             with  user authentication, registration, book browsing, and cart management for basic 
-                             understanding of the functionalites.
+                        {/* <FaBookAtlas id="react-icon"/> */}
+                        <a href="https://shaikmaimoon.github.io/my-new-app/" target="_blank" rel="noreferrer"><FaBookAtlas id="react-icon"/></a>
+                        <h5>Portfolio</h5>
+                        <p>o Developed a visually appealing portfolio featuring a showcase of projects, skills, and work, 
+                            utilizing creative design and animations to enhance user engagement.<br></br>o Demonstrated proficiency in web development, design, 
+                            and deployment by creating an interactive and polished online presence through the portfolio.
+                            <br />
+                            o Successfully hosted the portfolio on GitHub Pages, ensuring accessibility and easy sharing for potential employers and collaborators.
+                        </p>
+                    </div>
+                </div>
+                <div className="project-des"
+                data-aos="zoom-out"
+                data-aos-duration="1000">
+                    <div>
+                        {/* <FaPython id="react-icon"></FaPython> */}
+                        <a href="https://hrlabs.in/" target="_blank" rel="noreferrer"><FaBookAtlas id="react-icon"/></a>
+                        <h5>HRlabspage </h5>
+                        <p>o Enhance user experience and navigation for improved usability, ensure full responsiveness across all devices.<br></br>
+                            o As a Developer at hrlabs, I played a key role in the development of  HRlabs website. 
+                            Leveraging my expertise in React.js, HTML, and CSS, I collaborated with the design team 
+                            to implement a user-friendly and 
+                            visually appealing website for HR Labs, a human resource consulting firm.<br></br>
+                            o Successfully launched the hrlabs.in website, contributing to the company's online presence and 
+                            brand awareness in the HR consulting industry.
+
                         </p>
                     </div>
                 </div>
